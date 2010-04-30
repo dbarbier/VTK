91ee05d133498b8eee04652ee4f1d302a682b0d4
Denis Barbier <bouzim@gmail.com>
2010-04-30 22:54:16 +0200

    Improve Print() to help debugging
    See http://www.vtk.org/Bug/view.php?id=7886

--- Wrapping/vtkParseJava.c
+++ Wrapping/vtkParseJava.c
@@ -709,7 +709,7 @@ void vtkParseOutput(FILE *fp, FileInfo *data)
     /* Add the PrintRevisions method to vtkObject. */
     fprintf(fp,"  public native String PrintRevisions();\n");
     /* Add the default toString from java object */
-    fprintf(fp,"  public String toString() { return Print(); }\n");
+    fprintf(fp,"  public String toString() { return getClass().getName()+\"@\"+Integer.toHexString(hashCode())+\" \"+Print(); }\n");
     }
 
   if (!strcmp("vtkObject",data->ClassName))
